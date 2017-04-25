46930c5295996f7fa51565fb49953bd35101911d
Christian Frauenstein <cfrauenstein@gmail.com>
2017-04-25 23:25:43 +0200

    First commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import THREE = require("three");
+
+/**
+ * Sums 2 numbers
+ */
+
+export function main(){
+
+    console.log(THREE);
+    return;
+
+}
+export function sum(a: number, b: number) {
+    return a + b;
+}
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+require('awesome-typescript-loader');
+var path = require('path');
+
+module.exports = {
+        entry: './src/index.ts',
+        output: {
+                filename: 'bundle.js',
+                path: path.resolve(__dirname, 'dist')
+        },
+
+        // Currently we need to add '.ts' to the resolve.extensions array.
+        resolve: {
+                extensions: ['.ts', '.tsx', '.js', '.jsx']
+        },
+
+        // Source maps support ('inline-source-map' also works)
+        devtool: 'source-map',
+
+        module: {
+                loaders: [
+                        {
+                                test: /\.tsx?$/,
+                                loader: 'awesome-typescript-loader'
+                        }
+                ]
+        },
+
+        plugins: [
+                new HtmlWebpackPlugin()
+        ]
+};
